3f4eec7960a3332e2f7f32db4ab4862b285853a2
Nijan Adhikari <nijanadhikari@Nijans-MacBook-Air.local>
2024-12-05 02:50:00 -0800

    added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,12 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        <script
+          data-trigger="custom"
+          src="https://run.confettipage.com/here.js"
+          data-confetticode="U2FsdGVkX19uABdppwxmDocvJ8QmRUAfxQEyt98vKzJVNlj48rkFojUck6Rq9o1YcUYvUlSlS0fZB7WkKfVdKkUz1aD0ZgLz5eVWEOT01BzpwyOu1AASiH246AGM8cnrV2MIRLaqP5cOr8Y8RplrmkdCngV/GxcZoSBKbjIuwKmBPHBP76J0lowSy0+FyHdzQ3u7xnU3Tazv9cJwyOS2LCcO+njYkffY9ZBG9IAln64xiBOnfe/dSJfMczkNBOZmu/BSD95kVbMQ6+mIoqGff1oCQ3Oxy+kTSXKyhRbWOrAV/SeUDo+IuSCYgVOGk+Bl4wYVgO8mOGKsGBFmsjoCGrlZuS7UM6rNTC3a1a7zv3jh1b7njWfawcVntBeBze9SUO+Y0YS0c8kUz6EhokWKKl8kiVdrDAJ+1Nq+r89hvhnGWH859HZAgy84QOpV9Srt7WPrlBE+SKMg2QKGOWG+ljMf6UdcaQGBEZVkrc34NcCjmrQyWrCpA4tTmAhuDuAWzCiuvhux5szSLlQOFmkqZs/KT90tMuIH8s/Xf00smIRM1mFAk24PVbV6T+Vz1Ab+AUz++ozSib4Ar73kJ2Thhb7PpwnPupL4xydQQyzA5L+tlMAJiOEpK2FWv1v2ngrCwmE/xtrQUq7xuYYF/3Kw0+Swiq0Xcnlj2iB56T2Kxc4="
+        ></script>
+        ;
       </body>
     </Html>
   );
